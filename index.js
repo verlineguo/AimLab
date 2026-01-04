2dbd6136ab35d1e087a7f5e5800f025d7140001e
MarcoRafael24 <marcorafael234@gmail.com>
2026-01-04 20:45:02 +0700

    bola target + crosshair

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 100);
@@ -8,30 +7,52 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 cam.position.z = 10;
 
+// buat lighting
+const ambient = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambient);
 
-let light = new THREE.PointLight(0xffffff, 30);
-light.position.set(0, 4, 2);
-scene.add(light);
+const light1 = new THREE.PointLight(0xffffff, 2);
+light1.position.set(5, 5, 5);
+scene.add(light1);
 
-let light2 = new THREE.PointLight(0xffffff, 30);
-light2.position.set(0, -4, 2);
+const light2 = new THREE.PointLight(0xffffff, 1.5);
+light2.position.set(-5, -5, 5);
 scene.add(light2);
 
 
+// bola target biru
+const targetGeo = new THREE.SphereGeometry(0.18, 16, 16);
+const targetMat = new THREE.MeshStandardMaterial({
+    color: 0x0077ff
+});
+const ball = new THREE.Mesh(targetGeo, targetMat);
 
-const plane = new THREE.PlaneGeometry(10,10);
-const mat = new THREE.MeshBasicMaterial({color : 0xffffff});
-const mesh = new THREE.Mesh(plane, mat);
-scene.add(mesh);
-mesh.position.set(0, 0, -1);
+// posisi random
+ball.position.x = (Math.random() - 0.5) * 6;
+ball.position.y = (Math.random() - 0.5) * 4;
+ball.position.z = Math.random() * -6;
+scene.add(ball);
 
-let control = new OrbitControls(cam, renderer.domElement);
-scene.add(control);
+// crosshair
+const crosshairMat = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const size = 0.01;
 
+const crosshairPoints = [
+    new THREE.Vector3(-size, 0, 0),
+    new THREE.Vector3(size, 0, 0),
+    new THREE.Vector3(0, -size, 0),
+    new THREE.Vector3(0, size, 0)
+];
 
-function draw() {
-    control.update();    
+const crosshairGeo = new THREE.BufferGeometry().setFromPoints(crosshairPoints);
+const crosshair = new THREE.LineSegments(crosshairGeo, crosshairMat);
+
+crosshair.position.z = -1;
+cam.add(crosshair);
+scene.add(cam);
+
+function animate() {
     renderer.render(scene, cam);
-    requestAnimationFrame(draw);
+    requestAnimationFrame(animate);
 }
-draw();
+animate();
